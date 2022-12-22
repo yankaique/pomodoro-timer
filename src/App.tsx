9cc122a3a1ec68a255673baa68e41d91e787bfce
Yan Marinho <yan.marinho@pixter.com.br>
2022-12-22 07:09:45 -0300

    ✨ create routes

--- src/App.tsx
+++ src/App.tsx
@@ -1,16 +1,12 @@
 import { ThemeProvider } from 'styled-components'
-import { Button } from './components'
 import { defaultTheme } from './styles/themes/default'
 import { GlobalStyle } from './styles/global'
+import { Router } from './routes'
 
 export function App() {
   return (
     <ThemeProvider theme={defaultTheme}>
-      <h1>Hello Word</h1>
-      <Button variant="primary" />
-      <Button variant="danger" />
-      <Button variant="secondary" />
-      <Button variant="sucess" />
+      <Router />
       <GlobalStyle />
     </ThemeProvider>
   )
---
+++ src/routes/index.tsx
@@ -0,0 +1,17 @@
+import { Routes, Route, BrowserRouter } from 'react-router-dom'
+import { Home } from '../pages/home'
+import { History } from '../pages/history'
+import { DefaultLayout } from '../layouts/DefaultLayout'
+
+export function Router() {
+  return (
+    <BrowserRouter>
+      <Routes>
+        <Route path="/" element={<DefaultLayout />}>
+          <Route path="/" element={<Home />} />
+          <Route path="/history" element={<History />} />
+        </Route>
+      </Routes>
+    </BrowserRouter>
+  )
+}
